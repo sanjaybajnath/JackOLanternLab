32cc40d9d3e3ad891577dda89fad00c82eddb8c1
sanjaybajnath <sbajnath6151@bths.edu>
2018-11-27 14:35:52 -0500

    yes

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -10,10 +10,24 @@ public class JackOLantern {
     }
 
     public void fill(String str){
+        for(int i = 0; i< this.faceFeatures.length;i++){
+            for(int j=0;j<this.faceFeatures[i].length;j++){
+                this.faceFeatures[i][j]= str;
+            }
+        }
         
     }
 
     public String toString(){
-
+        String image = "";
+        for(int i = 0; i< this.faceFeatures.length;i++){
+            for(int j=0;j<this.faceFeatures[i].length;j++){
+                image += this.faceFeatures[i][j];
+            }
+            image = image + "\n";
+        }
+        return image;
     }
+
+
 }
