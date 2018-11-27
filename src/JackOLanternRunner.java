1165d3cf9971ff8a045a09e56a83bd12efb112d2
sanjaybajnath <sbajnath6151@bths.edu>
2018-11-27 14:41:35 -0500

    yes

--- src/JackOLanternRunner.java
+++ src/JackOLanternRunner.java
@@ -4,16 +4,16 @@ public class JackOLanternRunner {
         String[][] face = new String[5][8];
         JackOLantern face1 = new JackOLantern(face);
 
-        face1.fill("X");
+        face1.fill(".");
         face1.edit(" ",0,0);
         face1.edit(" ",4,7);
         face1.edit(" ",0,7);
         face1.edit(" ",4,0);
 
-        face1.edit("0",1,2);
-        face1.edit("0",1,5);
-        face1.edit("^",2,3);
-        face1.edit("^",2,4);
+        face1.edit("O",1,2);
+        face1.edit("O",1,5);
+        face1.edit("(",2,3);
+        face1.edit(")",2,4);
         face1.edit("|",3,1);
         face1.edit("|",3,2);
         face1.edit("_",3,3);
